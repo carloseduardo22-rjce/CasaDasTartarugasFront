288ebba4126766ce98766a5f9baed8f07fe74436
Carlos Eduardo <Carlosedu44@outlook.com>
2024-06-27 01:55:16 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  imports: [RouterOutlet, CommonModule, HomeComponent],
+  templateUrl: './app.component.html',
+  styleUrls: [],
 })
+
 export class AppComponent {
   title = 'ProjectHotel';
 }
